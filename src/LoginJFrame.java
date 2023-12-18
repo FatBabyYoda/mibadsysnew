f78fbd4a3ca99f010c1b23b8deb1495f6cb86ada
DubberOG <154241815+DubberOG@users.noreply.github.com>
2023-12-18 16:32:53 +0100

    abc

--- src/LoginJFrame.java
+++ src/LoginJFrame.java
@@ -252,7 +252,7 @@ private InfDB idb;
         //</editor-fold>
         //</editor-fold>
         //</editor-fold>
-
+        
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
